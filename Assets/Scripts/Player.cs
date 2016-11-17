5c901520c393fd9cf65db64263b23ce1cce35d3d
Jmeas <jellyes2@gmail.com>
2016-11-16 21:46:35 -0800

    Update Player boundary checking to use Coroutines
    Resolves #5

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,18 +1,27 @@
 ﻿public class Player : UnityEngine.MonoBehaviour {
   public UnityEngine.Rigidbody rb;
+  UnityEngine.Vector3 startPosition = new UnityEngine.Vector3(0f, 17f, 0f);
+  UnityEngine.Vector3 stationary = new UnityEngine.Vector3(0, 0, 0);
 
 	void Start() {
     rb = this.GetComponent<UnityEngine.Rigidbody>();
+    StartCoroutine(CheckOutOfBounds());
 	}
-	
-	void Update() {
-    this.CheckBounds();
-  }
 
-  void CheckBounds() {
-    if (this.transform.position.y < -6) {
-      rb.velocity = new UnityEngine.Vector3(0, 0, 0);
-      this.transform.position = new UnityEngine.Vector3(0f, 17f, 0f);
+  public System.Collections.IEnumerator CheckOutOfBounds() {
+    // This checks the boundaries always. Eventually, we'll want to turn this off
+    // under certain conditions, like when the player doesn't have control over
+    // the player's movement.
+    while(true) {
+      if (this.transform.position.y < -6) {
+        // Reset the player's velocity and their position
+        rb.velocity = stationary;
+        this.transform.position = startPosition;
+      }
+
+      // The player bounds are pretty loosely defined, so it's fine to check on
+      // them a little less frequently.
+      yield return new UnityEngine.WaitForSeconds(0.1f);
     }
   }
 }
